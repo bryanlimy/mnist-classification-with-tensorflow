74506810ba6e876c39e06cf519a96d3c644d7b8a
Bryan Li <bryanlimy@gmail.com>
2017-04-06 16:30:10 -0400

    added mnist_deep for fun

--- trainer.py
+++ trainer.py
@@ -7,6 +7,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 
+FLAGS = None
+
 def main(_):
 	# import MINST data
 	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
