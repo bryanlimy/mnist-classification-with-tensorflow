d29130b2fae9ef212f49d871ba8cfb7698cdf2e4
Bryan Li <bryanlimy@gmail.com>
2017-04-07 20:38:47 -0400

    moved extract data to data.py

--- data/data.py
+++ data/data.py
@@ -1,2 +1,11 @@
+# Download MINST data
+from tensorflow.examples.tutorials.mnist import input_data
+
 class Data:
+	def __init__(self):
+		# import MINST data
+		self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	def get_trains_input(self):
+		return self.mnist
+
 	
--- trainer.py
+++ trainer.py
@@ -6,37 +6,34 @@ import argparse, sys, os
 # ignore CPU warning
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-# Download MINST data
-from tensorflow.examples.tutorials.mnist import input_data
-
 import tensorflow as tf
+from data.data import *
 
 FLAGS = None
 
 def main(_):
-	# import MINST data
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	mnist = Data().get_trains_input()
 
-	# # create model
-	# x = tf.placeholder(tf.float32, [None, 784])
-	# W = tf.Variable(tf.zeros([784, 10]))
-	# b = tf.Variable(tf.zeros([10]))
-	# y = tf.nn.softmax(tf.matmul(x, W) + b)
+	# create model
+	x = tf.placeholder(tf.float32, [None, 784])
+	W = tf.Variable(tf.zeros([784, 10]))
+	b = tf.Variable(tf.zeros([10]))
+	y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-	# # define loss and optimizer
-	# y_ = tf.placeholder(tf.float32, [None, 10])
-	# cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+	# define loss and optimizer
+	y_ = tf.placeholder(tf.float32, [None, 10])
+	cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-	# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-	# # launch model
-	# sess = tf.InteractiveSession()
-	# tf.global_variables_initializer().run()
+	# launch model
+	sess = tf.InteractiveSession()
+	tf.global_variables_initializer().run()
 
-	# # train model 1000 times
-	# for _ in range(1000):
-	# 	batch_xs, batch_ys = mnist.train.next_batch(100)
-	# 	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	# train model 1000 times
+	for _ in range(1000):
+		batch_xs, batch_ys = mnist.train.next_batch(100)
+		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 	# test trained model
 	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
