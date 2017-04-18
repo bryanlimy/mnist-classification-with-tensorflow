607b272e6a5b42374755f9c07b6da137ce76049d
Bryan Li <bryanlimy@gmail.com>
2017-04-18 14:26:49 -0400

    fixed not InvalidArgumentError

--- models/mnist.py
+++ models/mnist.py
@@ -2,21 +2,19 @@ import tensorflow as tf
 
 def model(features, targets, mode):
 	# create model
-	x = tf.placeholder(tf.float32, [None, 784])
 	W = tf.Variable(tf.zeros([784, 10]))
 	b = tf.Variable(tf.zeros([10]))
-	y = tf.nn.softmax(tf.matmul(x, W) + b)
+	prediction = tf.nn.softmax(tf.matmul(features, W) + b)
 
-	predictions_dict = {"results": y}
+	predictions_dict = {"results": prediction}
 
 	# define loss and optimizer
-	y_ = tf.placeholder(tf.float32, [None, 10])
-	cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+	cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=targets, logits=prediction))
 
 	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 	eval_metric_ops = {
-		"rmse": tf.metrics.root_mean_squared_error(tf.cast(targets, tf.float32), y)
+		"accuracy": tf.metrics.accuracy(labels=targets, predictions=prediction, weights=None, metrics_collections=None, updates_collections=None, name=None)
 	}
 	return tf.contrib.learn.ModelFnOps(
 		mode=mode,
